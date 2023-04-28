501e211ca7a04381efa3e7d84fb692d75137306c
Todd McLeod <tuddleymc@gmail.com>
2023-04-27 22:38:17 -0400

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GoesToEleven/puppy
 
 go 1.20
+
+require github.com/GoesToEleven/dog v0.0.0-20230428023317-90bef1c76cb9 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/GoesToEleven/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
